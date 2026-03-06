f20768ec6d08ec7ce0681a62e2ecb338a75b164e
Zhang Wei <zhangwei@dev.com>
2026-03-06 09:00:00 +0800

    feat: add smart-interview-interview module with all dependencies

--- smart-interview-interview/src/main/java/com/smartinterview/interview/InterviewApplication.java
+++ smart-interview-interview/src/main/java/com/smartinterview/interview/InterviewApplication.java
@@ -4,18 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-/**
- * 面试服务启动类
- */
-@SpringBootApplication(scanBasePackages = "com.smartinterview")
+@SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @MapperScan("com.smartinterview.interview.mapper")
 public class InterviewApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(InterviewApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(InterviewApplication.class, args); }
 }
